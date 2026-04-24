4a4319555c103d647aea7ca2755ce7cfb8c8233c
Gabriel <santos.gabriel@aluno.ufca.edu.br>
2026-04-24 14:43:25 -0300

    feat: convert model to `.tflite`

--- optimize_model.py
+++ optimize_model.py
@@ -1,4 +1,14 @@
-import tensorflow as tf
 import os
 
-#insira seu código aqui
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+
+keras_model = load_model('mnist_model.h5')
+
+model = tf.lite.TFLiteConverter.from_keras_model(keras_model)
+model.optimizations = [tf.lite.Optimize.DEFAULT]
+
+tflite_model = model.convert()
+
+with open("mnist_model.tflite", "wb") as f:
+    f.write(tflite_model)
